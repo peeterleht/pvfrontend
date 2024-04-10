457078fd47a4ec51e7faae4f34a7076cb97f44b2
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-10 15:05:18 +0300

    LogInModal cancelled

--- src/components/Advert.vue
+++ src/components/Advert.vue
@@ -1,7 +1,62 @@
 <template>
-  REKLAAM
+  <div class="row">
+    <div class="col-sm-6" style="display: flex; align-items: flex-start;">
+      <div>
+        <p>Projektijuhtimine ei ole kunagi nii lihtne olnud</p>
+
+      </div>
+      <div>
+        <img src="../assets/smile.webp" class="img-fluid" style="width: 600px; height: auto; margin-right: 20px;" />
+      </div>
+    </div>
+  </div><div class="row">
+    <div class="col-sm-6" style="display: flex; align-items: flex-start;">
+      <div>
+        <p>Jalgi projekti hetkeseisu visuaalselt</p>
+
+      </div>
+      <div>
+        <img src="../assets/smile.webp" class="img-fluid" style="width: 600px; height: auto; margin-right: 20px;" />
+      </div>
+    </div>
+  </div><div class="row">
+    <div class="col-sm-6" style="display: flex; align-items: flex-start;">
+      <div>
+        <p>Planeeri ylesandeid</p>
+
+      </div>
+      <div>
+        <img src="../assets/smile.webp" class="img-fluid" style="width: 600px; height: auto; margin-right: 20px;" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
 </script>
+
+<style>
+.row {
+  display: flex;
+  justify-content: center;
+}
+
+.col {
+  max-width: 800px;
+  width: 100%;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+h1 {
+  margin-top: 0;
+  font-size: 24px;
+}
+
+p {
+  font-size: 16px;
+}
+</style>
