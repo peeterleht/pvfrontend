ead157f8d15501282b32fc386a4eea19a07b2812
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-10 13:27:38 +0300

    FrontPageHeader.vue WIP

--- src/components/FrontPageHeader.vue
+++ src/components/FrontPageHeader.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>PÄIS</div>
+
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">ProjektVisualiseeritud</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Võimalused</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Maksumus</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Projektide Näited</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Tagasiside</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <button class="btn btn-outline-success" type="submit">Logi sisse</button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
