97c7d522d0fd42fb4bf6a5dede168bb9536d38c7
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-10 15:26:24 +0300

    advert and tryout mocks done

--- src/components/TryOut.vue
+++ src/components/TryOut.vue
@@ -1,7 +1,16 @@
 <template>
-  PROOVI
+  <div class="border p-3"> <!-- Apply border utility class -->
+    <div class="row">
+      <div class="col">
+        <h3>Saad tasuta proovida meie rakendust</h3>
+      </div>
+      <div class="col">
+        <button class="btn btn-primary">proovi tasuta</button> <!-- Added btn class for styling -->
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-
+import Advert from "@/components/Advert.vue";
 </script>
