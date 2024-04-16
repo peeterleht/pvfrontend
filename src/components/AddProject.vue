1dcb75106d406175ddff2e304ecba435ad64e2b3
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-16 16:10:22 +0300

    sendPostProject added AddProject.vue

--- src/components/AddProject.vue
+++ src/components/AddProject.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="mb-3">
     <label for="projectCode" class="form-label">Projekti number</label>
-    <input v-model="projectCode" type="text" class="form-control" id="projectCode">
+    <input v-model="projectInfo.projectCode" type="text" class="form-control" id="projectCode">
   </div>
   <div class="mb-3">
     <label for="projectName" class="form-label">Projekti nimi</label>
-    <input v-model="projectName" type="password" class="form-control" id="projectName">
+    <input v-model="projectInfo.projectName" type="password" class="form-control" id="projectName">
   </div>
   <div>
     <label for="projectClient" class="form-label">Klient</label>
-    <input v-model="projectClient" type="text" class="form-control" id="projectClient">
+    <input v-model="projectInfo.projectClient" type="text" class="form-control" id="projectClient">
   </div>
   <div class="mb-3">
     <label for="projectBankLink" class="form-label">Projektipanga link</label>
-    <input v-model="projectBankLink" type="password" class="form-control" id="projectBankLink">
+    <input v-model="projectInfo.projectBankLink" type="password" class="form-control" id="projectBankLink">
   </div>
-  <button @click="" class="btn btn-outline-dark">Add Project</button>
+  <button @click="sendPostProject" class="btn btn-outline-dark">Add Project</button>
 </template>
 
 
@@ -24,12 +24,26 @@ export default {
   name: "AddProject",
   data() {
     return {
-      projectCode: '',
-      projectName: '',
-      projectClient: '',
-      projectBankLink: ''
+      projectInfo: {
+        projectCode: '',
+        projectName: '',
+        projectClient: '',
+        projectBankLink: ''
+      },
     }
   },
+  methods: {
+    sendPostProject(){
+      this.$http.post("/project", this.projectInfo
+      ).then(response => {
+        alert("yes")
+      }).catch(error => {
+        const errorResponseBody = error.response.data
+      })
+    },
+
+
+  }
 }
 
 </script>
