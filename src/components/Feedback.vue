7f25cfaed1719025fabaeaa48861f771262dcff1
Se <s@a.ee>
2024-04-16 21:20:07 +0300

    WIP front page

--- src/components/Feedback.vue
+++ src/components/Feedback.vue
@@ -1,7 +1,55 @@
 <template>
-  TAGASISIDE
+  <h1 style="text-align: start;">Kasutajad meist räägivad</h1>
+  <div class="row" style="margin-bottom: 100px;">
+    <div class="col-3 list-group text-start">
+      <a class="list-group-item list-group-item-action list-group-item-dark h-100">
+        <div class="d-flex w-100 justify-content-between">
+          <h2 class="mb-1">PEETER</h2>
+        </div>
+        <p class="mb-1">Lihtsalt parim projektijuhtimise platvorm maailmas!</p>
+      </a>
+    </div>
+    <div class="col-3 list-group text-start">
+      <a class="list-group-item list-group-item-action list-group-item-dark h-100">
+        <div class="d-flex w-100 justify-content-between">
+          <h2 class="mb-1">JOONAS</h2>
+        </div>
+        <p class="mb-1">Olen ka varem projektijuhtimisega kokku puutnud. Oleks see süsteem tol ajal, oleksid
+          pudelikaelad projektis ennustatud õigel ajal.</p>
+      </a>
+    </div>
+    <div class="col-3 list-group text-start">
+      <a class="list-group-item list-group-item-action list-group-item-dark h-100">
+        <div class="d-flex w-100 justify-content-between">
+          <h2 class="mb-1">SERGEI</h2>
+        </div>
+        <p class="mb-1">Lõppude lõpuks päriselt näen projekti.</p>
+      </a>
+    </div>
+    <div class="col-3 list-group text-start">
+      <a class="list-group-item list-group-item-action list-group-item-dark h-100">
+        <div class="d-flex w-100 justify-content-between">
+          <h2 class="mb-1">RAIN</h2>
+        </div>
+        <p class="mb-1">Mis värk on?</p>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script setup>
 
 </script>
+
+<style>
+h1 {
+  margin-top: 0;
+  font-size: 24px;
+  font-family: "Artifakt Element Black", serif;
+}
+
+h2 {
+  margin-top: 0;
+  font-size: 12px;
+}
+</style>
