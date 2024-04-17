147d57147e57f77d7b24ea8ce2f48bcd37ced08f
PEETER_AUGUST <peeterlehtz@gmail.com>
2024-04-17 09:49:14 +0300

    AddProject.vue Base made

--- src/components/AddProject.vue
+++ src/components/AddProject.vue
@@ -5,7 +5,7 @@
   </div>
   <div class="mb-3">
     <label for="projectName" class="form-label">Projekti nimi</label>
-    <input v-model="projectInfo.projectName" type="password" class="form-control" id="projectName">
+    <input v-model="projectInfo.projectName" type="text" class="form-control" id="projectName">
   </div>
   <div>
     <label for="projectClient" class="form-label">Klient</label>
@@ -13,7 +13,7 @@
   </div>
   <div class="mb-3">
     <label for="projectBankLink" class="form-label">Projektipanga link</label>
-    <input v-model="projectInfo.projectBankLink" type="password" class="form-control" id="projectBankLink">
+    <input v-model="projectInfo.projectBankLink" type="text" class="form-control" id="projectBankLink">
   </div>
   <button @click="sendPostProject" class="btn btn-outline-dark">Add Project</button>
 </template>
